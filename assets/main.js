b451c9209abc907f0c5b1a6d339367787f769783
Daniel <angelicodaniel93@gmail.com>
2022-10-19 15:00:45 +0200

    ci provo dopo a fare il form

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,17 @@ console.log('KmPercorso')
 
 let age = parseInt(prompt('Quanti anni hai?'))
 console.log('userAge')
+
+let totkm = km * MoneyPerKm;
+console.log('tot')
+
+// function generaPrezzo(){
+
+//     console.log('ho attivato la funziona generaPrezzo')
+
+
+//     let valuekm = document.getElementById('km').value;
+
+//     let age =
+
+// }
