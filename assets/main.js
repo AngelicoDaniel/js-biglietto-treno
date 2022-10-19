53e344c3d8c07de40ee646f5acf4e4440311b7f8
Daniel <angelicodaniel93@gmail.com>
2022-10-19 14:49:50 +0200

    km & age

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,8 @@
 const MoneyPerKm = 0.21;
 console.log('MoneyPerKm')
+
+let km = parseInt(prompt('Quanti km vuole percorrere?'))
+console.log('KmPercorso')
+
+let age = parseInt(prompt('Quanti anni hai?'))
+console.log('userAge')
