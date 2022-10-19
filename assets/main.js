4c1ebaebc124bcb947de5c9fc0c8cf64c351ba46
Daniel <angelicodaniel93@gmail.com>
2022-10-19 15:17:31 +0200

    if

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-const MoneyPerKm = 0.21;
+const MoneyPerKm = '0.21';
 console.log('MoneyPerKm')
 
 let km = parseInt(prompt('Quanti km vuole percorrere?'))
@@ -7,8 +7,28 @@ console.log('KmPercorso')
 let age = parseInt(prompt('Quanti anni hai?'))
 console.log('userAge')
 
-let totkm = km * MoneyPerKm;
-console.log('tot')
+let price = km * MoneyPerKm;
+console.log('price')
+
+if (age < 18) {
+    price - 20%
+    console.log('minorenne')
+} else if (age > 65){
+    price - 40%
+    console.log('over65')
+} else {
+    finalPrice = price
+    console.log('default')
+}
+
+
+
+
+
+
+
+
+
 
 // function generaPrezzo(){
 
